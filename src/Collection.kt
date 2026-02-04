de010f6c04b419eb9c70c05c099e8a5e20219ebc
Gabrovez <gabrovez321@gmail.com>
2026-02-04 03:40:02 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -6,14 +6,39 @@ val newSolarSystems = arrayOf(
     "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
 
 fun main() {
-    println(solarSystems[0])
-    println(solarSystems[1])
-    println(solarSystems[2])
-    solarSystems[3] = "Little Earth"
-    println(solarSystems[3])
-    println(solarSystems[4])
-    println(solarSystems[5])
-    println(solarSystems[6])
-    println(solarSystems[7])
-    println(newSolarSystems[8])
+
+    val solarSystem = mutableListOf(
+        "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
+    )
+    solarSystem.add("Pluto")
+    solarSystem.add(3,"Theia")
+    solarSystem[3] = "Future Moon"
+    println(solarSystem[3])
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
+//    val solarSystem = listOf(
+//        "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
+//    )
+//    println(solarSystem.size)
+//    println(solarSystem[2])
+//    println(solarSystem.get(3))
+//    println(solarSystem.indexOf("Earth"))
+//    println(solarSystem.indexOf("Pluto"))
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
+
+//    println(solarSystems[0])
+//    println(solarSystems[1])
+//    println(solarSystems[2])
+//    solarSystems[3] = "Little Earth"
+//    println(solarSystems[3])
+//    println(solarSystems[4])
+//    println(solarSystems[5])
+//    println(solarSystems[6])
+//    println(solarSystems[7])
+//    println(newSolarSystems[8])
 }
